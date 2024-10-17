bdf61bccc8c74d72235e0dfda0b4ef4fb2179476
I_R_A_J <120247061+I-r-a-j@users.noreply.github.com>
2024-10-17 11:11:17 +0330

    Update dashboard.py
    modify comments

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ from supabase import create_client, Client
 # Set page configuration
 st.set_page_config(layout="wide", page_title="Alpha Zone 2023 Dashboard")
 
-# Your Supabase URL and key (replace with your actual values)
+# Supabase URL and key 
 url = "https://cukweowdhbzfjqqufwns.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1a3dlb3dkaGJ6ZmpxcXVmd25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzMzA4NzgsImV4cCI6MjA0MTkwNjg3OH0.9BpDBshjSB54tbLVQ7MuSn7FopnnOp63FS9ASJ5pgps"
 
